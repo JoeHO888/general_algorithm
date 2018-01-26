4b7ad4f693e5a9a79f2270ac9f694a5b495c6905
JoeHo <joe.hochotai@gmail.com>
2018-01-26 10:24:19 +0800

    Update travel_sales_man.py

--- travel_sales_man.py
+++ travel_sales_man.py
@@ -2,7 +2,7 @@ import numpy as np
 
 generation_size=100
 pop_size=100
-cities_size=10
+cities_size=4
 mutation_rate=0.1
 crossover_rate=0.8
 map_size=11
@@ -12,13 +12,14 @@ d={}
 for x in range(0,cities_size):
         d[int("{}".format(x))]=np.random.randint(map_size,size=(2,1))
 
+print(d)
 
 def mutate(children):
   for e in children:
-    for i in range(10):
+    for i in range(cities_size):
       if np.random.uniform(0,1,1)<mutation_rate:
         a=e[i]
-        s=np.random.randint(10,size=1)
+        s=np.random.randint(cities_size,size=1)
         e[i]=e[s]
         e[s]=a
   return children
@@ -27,7 +28,7 @@ def fitness(pop):
   fitness=np.zeros(pop_size)
   for j in range(pop_size):
     fitness[j]=np.sum([np.linalg.norm(d[pop[j,i]]-d[pop[j,i+1]]) for i in range(cities_size-1)])
-  return 1000-fitness
+  return 100-fitness
 
 
 def select(pop):
@@ -59,8 +60,8 @@ for i in range(generation_size):
   children=np.vstack((children_1,children_2))
   pop=mutate(children)
 
-#graph is needed
 
+#graph is needed
 
         
   
