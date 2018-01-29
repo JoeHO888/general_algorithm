a886f27102b2303a220abc05f18460b1b68b70aa
JoeHo <joe.hochotai@gmail.com>
2018-01-29 09:23:37 +0800

    Update travel_sales_man.py

--- travel_sales_man.py
+++ travel_sales_man.py
@@ -1,10 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 generation_size=100
-pop_size=100
-cities_size=10
+pop_size=500
+cities_size=20
 mutation_rate=0.02
-crossover_rate=0.8
+crossover_rate=0.2
 map_size=21
 
 d={}
@@ -28,24 +28,22 @@ def mutate(children):
 def fitness(pop):
   fitness=np.zeros(pop_size)
   for j in range(pop_size):
-    fitness[j]=np.sum([np.linalg.norm(d[pop[j,i]]-d[pop[j,i+1]]) for i in range(cities_size-1)])
-  return 1000-fitness
+    fitness[j]=np.exp(1.0/np.sum([np.linalg.norm(d[pop[j,i]]-d[pop[j,i+1]]) for i in range(cities_size-1)]))
+  return fitness
 
 
 def select(pop):
-  index=np.random.choice(pop_size, pop_size//2, replace=False,p=fitness(pop)/fitness(pop).sum()) 
+  index=np.random.choice(pop_size, pop_size, replace=True,p=fitness(pop)/fitness(pop).sum()) 
   return np.array([pop[i] for i in index]) 
 
-def crossover(dad,mom):
+def crossover(dad,pop):
   for i in range(dad.shape[0]):
     if np.random.uniform(0,1,1)<crossover_rate:
-      crossover_point=np.random.randint(0,cities_size,size=1)[0] #ok
-      suffix=mom[i,crossover_point:]
-      for e in suffix:
-        if e in dad[i,:crossover_point]:
-          candidates=set(dad[i,crossover_point:]) - set(suffix)
-          suffix[suffix == e] = np.random.choice(np.array(list(candidates)),1)
-      dad[i]=np.hstack((dad[i,:crossover_point],suffix))
+      i_ = np.random.randint(0, cities_size, size=1) 
+      cross_points = np.random.randint(0, 2, cities_size).astype(np.bool) #ok
+      keep_city=dad[i,~cross_points]
+      swap_city = pop[i_, np.isin(pop[i_].ravel(), keep_city, invert=True)]
+      dad[i,:] = np.concatenate((keep_city, swap_city))
   return dad
         
       
@@ -53,18 +51,12 @@ g=np.array([np.random.choice(cities_size,cities_size,replace=False) for i in ran
 
 for i in range(generation_size):
   print("gen: "+str(i))
-  hor=np.array([d[e][0] for e in g[np.argmin(fitness(g))]])
-  ver=np.array([d[e][1] for e in g[np.argmin(fitness(g))]])
+  hor=np.array([d[e][0] for e in g[np.argmax(fitness(g))]])
+  ver=np.array([d[e][1] for e in g[np.argmax(fitness(g))]])
   plt.figure(figsize=(20,10))
   plt.plot(x, y,"ro",ms=20)
   plt.plot(hor, ver,linewidth=3)
   plt.show() 
-  parent1=select(g)
-  parent2=select(g)
-  parent3=select(g)
-  parent4=select(g)
-  child_1=crossover(parent1,parent3)
-  child_2=crossover(parent2,parent4)
-  new_g=np.vstack((child_1,child_2))
-  g=mutate(new_g)
-  
+  parent=select(g)
+  children=crossover(parent,g)
+  g=mutate(children)
